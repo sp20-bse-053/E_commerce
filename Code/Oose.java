36bff3fcf956e41c8ab1e19ea555a9698418dbfc
sp20-bse-056 <92437716+sp20-bse-056@users.noreply.github.com>
2021-11-24 17:00:55 +0500

    Sum
    By Munawer

---
+++ Code/Oose.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oose;
+
+/**
+ *
+ * @author test
+ */
+public class Oose {
+
+    
+    public static void main(String[] args) {
+       newoose obj = new newoose();
+       obj.sum(12, 12);
+    }
+    
+}
